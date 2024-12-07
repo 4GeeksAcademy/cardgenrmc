215101c8199060d494518b2776878d0c52d29674
Ruben <167398241+rumacon@users.noreply.github.com>
2024-12-07 23:29:03 +0000

    ya

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = [
+    { palo: "♠︎", color: "black" },
+    { palo: "♣︎", color: "black" },
+    { palo: "♥︎", color: "red" },
+    { palo: "♦︎", color: "red" }
+  ];
+
+  let random1 = Math.floor(Math.random() * number.length);
+  let random2 = Math.floor(Math.random() * palos.length);
+
+  document.getElementById("carta").innerHTML = number[random1];
+
+  document.getElementById("icono").innerHTML = palos[random2].palo;
+  document.getElementById("icono2").innerHTML = palos[random2].palo;
+
+  document.getElementById("icono").style.color = palos[random2].color;
+  document.getElementById("icono2").style.color = palos[random2].color;
+  document.getElementById("carta").style.color = palos[random2].color;
 };
